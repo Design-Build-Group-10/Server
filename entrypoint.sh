ad1aea7955fc03f0b5209273496cd10df5f2ebed
Charles <1447393600@qq.com>
2024-10-05 01:54:43 +0800

    :bug:

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ WORKERS=$((CORE_COUNT * 2 + 1))
 #gunicorn config.asgi:application -w $WORKERS -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --log-level debug --access-logfile -
 
 # 启动 Gunicorn 服务器，使用 --preload 选项以减少内存占用和加快启动时间
-gunicorn config.asgi:application --preload -w $WORKERS -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --log-level debug --access-logfile -
+gunicorn config.asgi:application --preload -w $WORKERS -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --log-level debug --access-logfile - --timeout 1000
