e8d970a2677c695ddacd7367bbdde3394750675b
CharlesZZY <1447393600@qq.com>
2024-10-13 14:10:20 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 
 export BASE_DIR=
 
-CORE_COUNT=$(python -c "import os; print(os.cpu_count())")
+#CORE_COUNT=$(python -c "import os; print(os.cpu_count())")
 
-WORKERS=$((CORE_COUNT * 2 + 1))
+#WORKERS=$((CORE_COUNT * 2 + 1))
+
+WORKERS=3
 
 gunicorn config.asgi:application -w $WORKERS -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --log-level debug --access-logfile -
